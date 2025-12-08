6242bce716762d6e22814e449d57721a6d2357e6
Benedikt Buchert <benedikt@buchert.digital>
2025-12-08 20:01:02 +0100

    chore: base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://benedikt-buchert.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tracking_docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
