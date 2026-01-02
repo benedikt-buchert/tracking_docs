785e2bc9576915a846849513d21f0aa6f39459d8
Benedikt Buchert <benedikt@buchert.digital>
2026-01-02 15:18:50 +0100

    chore: add debug config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   transformIgnorePatterns: [
-    'node_modules/(?!(docusaurus-plugin-generate-schema-docs))',
+    'node_modules/(?!(docusaurus-plugin-generate-schema-docs|@apidevtools/json-schema-ref-parser))',
   ],
   moduleNameMapper: {
     '^@theme/CodeBlock$': '<rootDir>/jest.CodeBlock.mock.js',
