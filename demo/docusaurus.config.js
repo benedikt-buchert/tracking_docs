a05b04dbf5c0f89621ada9c2ba8c9eec4c9e9567
Benedikt Buchert <benedikt@buchert.digital>
2026-01-02 15:29:32 +0100

    feature: root url

--- demo/docusaurus.config.js
+++ demo/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://tracking-docs-demo.buchert.digital/',
+  url: 'https://tracking-docs-demo.buchert.digital/tracking_docs/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/tracking_docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
