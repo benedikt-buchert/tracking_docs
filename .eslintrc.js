d031ed238c3b9a033a13e67db11111f4d326e954
Benedikt Buchert <benedikt@buchert.digital>
2026-01-22 10:18:56 +0100

    chore: remove eempty file

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:jest/recommended',
-    'prettier',
+    'plugin:json-schema-validator/recommended',
+    'plugin:prettier/recommended',
   ],
   parser: '@babel/eslint-parser',
   parserOptions: {
@@ -14,14 +15,28 @@ module.exports = {
       presets: [require.resolve('@docusaurus/core/lib/babel/preset')],
     },
   },
-  plugins: ['react', 'react-hooks', 'jsx-a11y', 'jest', 'prettier'],
+  plugins: [
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+    'jest',
+    'prettier',
+    'json-schema-validator',
+  ],
   rules: {
     'prettier/prettier': 'error',
     'react/prop-types': 'off',
+    'json-schema-validator/no-invalid': 'error',
   },
   settings: {
     react: {
       version: 'detect',
     },
   },
+  overrides: [
+    {
+      files: ['*.json', '*.json5', '*.jsonc'],
+      parser: 'jsonc-eslint-parser',
+    },
+  ],
 };
