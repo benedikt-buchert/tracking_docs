a1d498d63cd962dc4da666a6c436d96887515530
Benedikt Buchert <benedikt@buchert.digital>
2026-02-03 10:44:41 +0100

    feature: allow toggeling of text wrap in the table
    Signed-off-by: Benedikt Buchert <benedikt@buchert.digital>

--- jest.config.js
+++ jest.config.js
@@ -4,10 +4,10 @@ module.exports = {
     'node_modules/(?!(docusaurus-plugin-generate-schema-docs|@apidevtools/json-schema-ref-parser))',
   ],
   moduleNameMapper: {
-    '^@theme/CodeBlock$': '<rootDir>/jest.CodeBlock.mock.js',
-    '^@theme/Heading$': '<rootDir>/jest.Heading.mock.js',
-    '^@theme/Tabs$': '<rootDir>/jest.Tabs.mock.js',
-    '^@theme/TabItem$': '<rootDir>/jest.TabItem.mock.js',
-    '\\.(css|less|sass|scss)$': '<rootDir>/jest.css.mock.js',
+    '^@theme/CodeBlock$': '<rootDir>/__mocks__/jest.CodeBlock.mock.js',
+    '^@theme/Heading$': '<rootDir>/__mocks__/jest.Heading.mock.js',
+    '^@theme/Tabs$': '<rootDir>/__mocks__/jest.Tabs.mock.js',
+    '^@theme/TabItem$': '<rootDir>/__mocks__/jest.TabItem.mock.js',
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/jest.css.mock.js',
   },
 };
--- packages/docusaurus-plugin-generate-schema-docs/components/PropertiesTable.js
+++ packages/docusaurus-plugin-generate-schema-docs/components/PropertiesTable.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SchemaRows from './SchemaRows';
 import TableHeader from './TableHeader';
+import WordWrapButton from './WordWrapButton';
 import { schemaToTableData } from '../helpers/schemaToTableData';
+import styles from './PropertiesTable.module.css';
 
 export default function PropertiesTable({ schema }) {
+  const [isWordWrapOn, setIsWordWrapOn] = useState(true);
   const tableData = schemaToTableData(schema);
 
   return (
-    <table>
-      <TableHeader />
-      <tbody>
-        <SchemaRows tableData={tableData} />
-      </tbody>
-    </table>
+    <div
+      className={`${styles.tableWrapper} ${
+        !isWordWrapOn ? styles.noWordWrap : ''
+      }`}
+    >
+      <div className={styles.buttonGroup}>
+        <WordWrapButton
+          onClick={() => setIsWordWrapOn((w) => !w)}
+          isEnabled={isWordWrapOn}
+        />
+      </div>
+      <table>
+        <TableHeader />
+        <tbody>
+          <SchemaRows tableData={tableData} />
+        </tbody>
+      </table>
+    </div>
   );
 }
