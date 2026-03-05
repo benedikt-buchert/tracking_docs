282dee68448d41f702b3d20473b076fdedda2e9a
benedikt-buchert <benedikt@mohrstade.de>
2026-03-05 20:08:18 +0100

    refactor(issue-53): extract normalized example model from ExampleDataLayer

--- packages/docusaurus-plugin-generate-schema-docs/components/ExampleDataLayer.js
+++ packages/docusaurus-plugin-generate-schema-docs/components/ExampleDataLayer.js
@@ -3,27 +3,23 @@ import CodeBlock from '@theme/CodeBlock';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 import Heading from '@theme/Heading';
-import { schemaToExamples } from '../helpers/schemaToExamples';
 import {
+  buildExampleModel,
   findClearableProperties,
-  generateSnippetForTarget,
-} from '../helpers/snippetTargets';
+} from '../helpers/exampleModel';
 
 export default function ExampleDataLayer({ schema, dataLayerName }) {
-  const exampleGroups = schemaToExamples(schema);
+  const model = buildExampleModel(schema, { dataLayerName });
+  const exampleGroups = model.variantGroups;
+  const targetId = model.targets[0].id;
 
   if (!exampleGroups || exampleGroups.length === 0) {
     return null;
   }
 
   // Handle the simple case of a single default example with no choices
-  if (exampleGroups.length === 1 && exampleGroups[0].property === 'default') {
-    const codeSnippet = generateSnippetForTarget({
-      targetId: 'web-datalayer-js',
-      example: exampleGroups[0].options[0].example,
-      schema,
-      dataLayerName,
-    });
+  if (model.isSimpleDefault) {
+    const codeSnippet = exampleGroups[0].options[0].snippets[targetId];
     return <CodeBlock language="javascript">{codeSnippet}</CodeBlock>;
   }
 
@@ -35,16 +31,9 @@ export default function ExampleDataLayer({ schema, dataLayerName }) {
             <code>{group.property}</code> options:
           </Heading>
           <Tabs>
-            {group.options.map(({ title, example }, index) => (
+            {group.options.map(({ title, snippets }, index) => (
               <TabItem value={index} label={title} key={index}>
-                <CodeBlock language="javascript">
-                  {generateSnippetForTarget({
-                    targetId: 'web-datalayer-js',
-                    example,
-                    schema,
-                    dataLayerName,
-                  })}
-                </CodeBlock>
+                <CodeBlock language="javascript">{snippets[targetId]}</CodeBlock>
               </TabItem>
             ))}
           </Tabs>
@@ -53,4 +42,5 @@ export default function ExampleDataLayer({ schema, dataLayerName }) {
     </>
   );
 }
+
 export { findClearableProperties };
